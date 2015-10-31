31a91c4af9adc9ad0ee41d9fa16692cac98549a0
craig-warren <craig_warren@me.com>
2015-10-31 21:14:33 +0000

    Update syntax of version number reading function.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,9 +68,7 @@ author = 'Craig Warren and Antonis Giannopoulos'
 #
 # The short X.Y version.
 # Read version number from gprMax/gprMax.py
-version = re.search('^__version__\s*=\s*\'(.*)\'',
-                    open('../../gprMax/gprMax.py').read(),
-                    re.M).group(1)
+version = re.search('^__version__\s*=\s*\'(.*)\'', open('../../gprMax/gprMax.py').read(), re.M).group(1)
 # The full version, including alpha/beta/rc tags.
 release = version + '(Bowmore)'
 
