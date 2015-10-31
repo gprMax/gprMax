f14983b912800f531edb79b5ae5d6b32f4fa8de2
craig-warren <craig_warren@me.com>
2015-10-31 21:10:16 +0000

    Updated to automatically read version number for docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,10 +68,9 @@ author = 'Craig Warren and Antonis Giannopoulos'
 #
 # The short X.Y version.
 # Read version number from gprMax/gprMax.py
-#version = re.search('^__version__\s*=\s*\'(.*)\'',
-#                    open('../../gprMax/gprMax/gprMax.py').read(),
-#                    re.M).group(1)
-version = '3.0.0b3'
+version = re.search('^__version__\s*=\s*\'(.*)\'',
+                    open('../../gprMax/gprMax.py').read(),
+                    re.M).group(1)
 # The full version, including alpha/beta/rc tags.
 release = version + '(Bowmore)'
 
