3d484f2f5848214fae042c6890d46d99658dec0a
Craig Warren <craig_warren@me.com>
2015-11-02 18:46:08 +0000

    Updated beta version to 3.0.0b4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,7 +69,7 @@ author = 'Craig Warren and Antonis Giannopoulos'
 # The short X.Y version.
 # Read version number from gprMax/gprMax.py
 #version = re.search('^__version__\s*=\s*\'(.*)\'', open('../../gprMax/gprMax.py').read(), re.M).group(1)
-version = '3.0.0b3'
+version = '3.0.0b4'
 # The full version, including alpha/beta/rc tags.
 release = version + '(Bowmore)'
 
