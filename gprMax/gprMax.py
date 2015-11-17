40aaa82c4aea2beaffcbb6f3186c7e4c9ec78a72
craig-warren <craig_warren@me.com>
2015-11-17 13:00:22 +0000

    Updated version to v.3.0.0b8.

--- gprMax/gprMax.py
+++ gprMax/gprMax.py
@@ -19,7 +19,7 @@
 """gprMax.gprMax: provides entry point main()."""
 
 # Set the version number here
-__version__ = '3.0.0b7'
+__version__ = '3.0.0b8'
 versionname = ' (Bowmore)'
 
 import sys, os, datetime, itertools, argparse
