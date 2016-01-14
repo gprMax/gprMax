ae923a22db3de8e97815e5455202af0a701370d0
Craig Warren <craig_warren@me.com>
2016-01-14 18:05:28 +0000

    Tidied imports.

--- gprMax/input_cmds_file.py
+++ gprMax/input_cmds_file.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with gprMax.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys, os
+import os, sys
 
 from gprMax.exceptions import CmdInputError
 from gprMax.utilities import ListStream
