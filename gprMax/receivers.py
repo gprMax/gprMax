142790fc27953188bdd9717c096cfd8193a26ce9
Craig Warren <craig_warren@me.com>
2016-05-06 12:23:14 +0100

    Changed to new-style classes.

--- gprMax/receivers.py
+++ gprMax/receivers.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with gprMax.  If not, see <http://www.gnu.org/licenses/>.
 
-class Rx:
+class Rx(object):
     """Receiever output points."""
     
     availableoutputs = ['Ex', 'Ey', 'Ez', 'Hx', 'Hy', 'Hz', 'Ix', 'Iy', 'Iz']
