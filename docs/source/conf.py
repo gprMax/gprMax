497a10d357c22b7f86ed092a3feb478cf780b86d
craig-warren <craig_warren@me.com>
2018-04-12 17:15:40 +0100

    Test logo in Latex docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -248,7 +248,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-# latex_logo = 'images/gprMax_logo.png'
+latex_logo = 'images/gprMax_logo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
