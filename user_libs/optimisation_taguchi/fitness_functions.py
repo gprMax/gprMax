0ca02137b328e700e2581103bcfdbdc023140388
Craig Warren <cwarren@C17776592.local>
2019-01-04 09:25:37 +0000

    Updated copyright date to 2019.

--- user_libs/optimisation_taguchi/fitness_functions.py
+++ user_libs/optimisation_taguchi/fitness_functions.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2018, Craig Warren
+# Copyright (C) 2015-2019, Craig Warren
 #
 # This module is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
 # To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
