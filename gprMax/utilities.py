0dbef429eee38e068418f61e4920bc09b7746fec
Craig Warren <cwarren@C17776592.local>
2019-11-18 14:13:27 +0000

    Updated CPU detection on Linux to account for different locales.

--- gprMax/utilities.py
+++ gprMax/utilities.py
@@ -295,7 +295,7 @@ def get_host_info():
             for line in cpuIDinfo.split('\n'):
                 if re.search('model name', line):
                     cpuID = re.sub('.*model name.*:', '', line, 1).strip()
-            allcpuinfo = subprocess.check_output("lscpu", shell=True, stderr=subprocess.STDOUT).decode('utf-8').strip()
+            allcpuinfo = subprocess.check_output("lscpu", shell=True, env=my_env, stderr=subprocess.STDOUT).decode('utf-8').strip()
             for line in allcpuinfo.split('\n'):
                 if 'Socket(s)' in line:
                     sockets = int(re.sub("\D", "", line.strip()))
