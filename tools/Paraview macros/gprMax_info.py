6c8d22207212aa3f466840f2ae33492faeaee9ae
craig-warren <craig_warren@me.com>
2020-04-24 11:39:01 +0100

    Updated reading of strings in binary file.

--- tools/Paraview macros/gprMax_info.py
+++ tools/Paraview macros/gprMax_info.py
@@ -40,15 +40,15 @@ srcs_pml = []
 rxs = []
 with open(model.FileName[0], 'rb') as f:
     for line in f:
-        if line.startswith('<Material'):
+        if line.startswith(b'<Material'):
             line.rstrip('\n')
             tmp = (int(ET.fromstring(line).text), ET.fromstring(line).attrib.get('name'))
             materials.append(tmp)
-        elif line.startswith('<Sources') or line.startswith('<PML'):
+        elif line.startswith(b'<Sources') or line.startswith(b'<PML'):
             line.rstrip('\n')
             tmp = (int(ET.fromstring(line).text), ET.fromstring(line).attrib.get('name'))
             srcs_pml.append(tmp)
-        elif line.startswith('<Receivers'):
+        elif line.startswith(b'<Receivers'):
             line.rstrip('\n')
             tmp = (int(ET.fromstring(line).text), ET.fromstring(line).attrib.get('name'))
             rxs.append(tmp)
