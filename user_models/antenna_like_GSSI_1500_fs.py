0ca6a3c7b3853ac1f040a1b916017874dd3da65f
craig-warren <craig_warren@me.com>
2020-05-18 15:56:59 +0100

    Testing rotation.

--- user_models/antenna_like_GSSI_1500_fs.py
+++ user_models/antenna_like_GSSI_1500_fs.py
@@ -11,7 +11,7 @@ dl = 0.001
 
 # Domain
 x = 0.250
-y = 0.188
+y = 0.250
 z = 0.220
 
 scene = gprMax.Scene()
@@ -27,9 +27,22 @@ scene.add(dxdydz)
 scene.add(time_window)
 
 # Import antenna model and add to model
-gssi_objects = antenna_like_GSSI_1500(0.125, 0.094, 0.100, resolution=dl)
+ant_pos = (0.125, 0.094, 0.100)
+gssi_objects = antenna_like_GSSI_1500(ant_pos[0], ant_pos[1], ant_pos[2], 
+                                      resolution=dl)
 for obj in gssi_objects:
+    obj.rotate('z', 90, origin=(ant_pos[0], ant_pos[1], ant_pos[2]))
     scene.add(obj)
 
+gv1 = gprMax.GeometryView(p1=(0, 0, 0), p2=(x, y, z),
+                          dl=(dl, dl, dl), filename='antenna_like_GSSI_1500',
+                          output_type='n')
+gv2 = gprMax.GeometryView(p1=(ant_pos[0] - 0.170/2, ant_pos[1] - 0.108/2, ant_pos[2] - 0.050), 
+                          p2=(ant_pos[0] + 0.170/2, ant_pos[1] + 0.108/2, ant_pos[2] + 0.010),
+                          dl=(dl, dl, dl), filename='antenna_like_GSSI_1500_pcb',
+                          output_type='f')
+scene.add(gv1)
+scene.add(gv2)
+
 # Run model
-gprMax.run(scenes=[scene], geometry_only=False, outputfile=fn, gpu=[0])
+gprMax.run(scenes=[scene], geometry_only=True, outputfile=fn, gpu=[0])
