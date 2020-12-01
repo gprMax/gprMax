06be5c25c087f7dbbf60b1ab5c18bad98d3a69ce
BerndAre <74766933+BerndAre@users.noreply.github.com>
2020-12-01 15:40:23 +0100

    first working example

--- user_libs/antennas/TEM_Horn.py
+++ user_libs/antennas/TEM_Horn.py
@@ -24,7 +24,7 @@ def horn_burr(x, y, z, resolution = 0.0005, rotation = 0, measurement ='monostat
     """
     L           = 0.2               # m
     L_plate     = 0.07
-    resolution  = 0.0005            # m
+    #resolution  = 0.0005            # m
     R           = 0.019978589752617
     Rside       = 0.011
     B           = 0.17                   # m
@@ -46,12 +46,12 @@ def horn_burr(x, y, z, resolution = 0.0005, rotation = 0, measurement ='monostat
     deltaB      = 0.02                  # m
     B2          = B + deltaB            # m
     lBalun      = 0.014                 # m
-    balunDiaBig = 0.011                 # m
+    balunDiaBig = 0.012                 # m
     balunDiaSmall = 0.008               # m
-    smaDia      = 0.001
-    smaTeflonDia= 0.005
-    smaSocketDia= 0.008
-    smaScrwDia  = 0.006
+    smaDia      = 0.001                 # m
+    smaTeflonDia= 0.005                 # m
+    smaSocketDia= 0.008                 # m
+    smaScrwDia  = 0.006                 # m
     sourceresistance = 200              # ohm
     
     # if resolution == 0.001:
@@ -95,28 +95,82 @@ def horn_burr(x, y, z, resolution = 0.0005, rotation = 0, measurement ='monostat
     # horn
     x_z = C1 * np.exp((zl*R))+C2      # horn part
     y_z = C1_side*np.exp(Rside*zl)+C2_side+(wfeed*1e3/2)-wfeed*1e3/2   # c1_side*efk^(R_side*v*u)+c2_side+(w_feed/2*v)-w_feed/2 # C1_SIDE*exp(R_SIDE*t)+C2_SIDE+(W_FEED/2mm)-W_FEED/2
-    x_alpah = W1*1e3/2+arcRadius*1e3*np.sin(alpha)+np.cos(slopeAngel)*arcRadius*1e3
-    y_alpah = B*1e3/2+deltaB*1e3/2*(alpha-arcStart)/arcDelta #B/2+(DELTA_B)/2*(t-ARC_START/1grd)/(ARC_DELTA/1grd)
+    x_alpha = W1*1e3/2+arcRadius*1e3*np.sin(alpha)+np.cos(slopeAngel)*arcRadius*1e3
+    y_alpha = B*1e3/2+deltaB*1e3/2*(alpha-arcStart)/arcDelta #B/2+(DELTA_B)/2*(t-ARC_START/1grd)/(ARC_DELTA/1grd)
     z_alpha = L*1e3+arcRadius*1e3*np.cos(alpha)-np.sin(slopeAngel)*arcRadius*1e3
 
-    # discret funcion
-    zzy, yy_z = disGeometryGprMax(zl, y_z, resolution*1e3)
-    zzx, xx_z = disGeometryGprMax(zl, x_z, resolution*1e3)
-    zx_a, x_za = disGeometryGprMax(z_alpha, x_alpah, resolution*1e3)
-    zy_a, y_za = disGeometryGprMax(z_alpha, y_alpah, resolution*1e3)
+    # discret horn funcion
+    zn, yn_z, xn_z = disGeometryGprMax(zl, y_z, x_z, resolution*1e3)
+    za, ya, xa = disGeometryGprMax(z_alpha, y_alpha, x_alpha, resolution*1e3)
+    
+    ### koordinates in gprMax-koordinates mm --> m ###
+    # horn
+    zn_real         = z-zn/1e3
+    xn_zreal_pos    = x + xn_z/1e3
+    xn_zreal_neg    = x - xn_z/1e3
+    yn_zreal_pos    = y + yn_z/1e3
+    yn_zreal_neg    = y - yn_z/1e3
+
+    # radius
+    za_real         = z - za/1e3
+    xa_real_pos     = x + xa/1e3
+    xa_real_neg     = x - xa/1e3
+    ya_real_pos     = y + ya/1e3
+    ya_real_neg     = y - ya/1e3
+    # plot function
+    # fig = plt.figure()
+    # ax = fig.gca(projection='3d')
+    # #plt.plot(zn_real, yn_zreal_pos, xn_zreal_pos, zn_real, yn_zreal_neg, xn_zreal_neg)
+    # #plt.show()
+    # plt.plot(za_real, ya_real_pos, xa_real_pos)
+    # plt.show()
     
     # build geometry
-    zz = np.append(zzy, zzx)
-    zz = np.sort(zz)
-    print('zzy:'+str(zzy)+'\n zzx'+str(zzx)+'\n zconnect and sorted:'+str(np.unique(zz)))
+    # horn
+    for n in range(0, len(zn_real)-1):
+        #print(zn_real[n], zn_real[n+1])
+        xup = np.amax([xn_zreal_pos[n], xn_zreal_pos[n+1]])+resolution
+        xlow = np.amin([xn_zreal_pos[n], xn_zreal_pos[n+1]])
+        yup = np.amax([yn_zreal_pos[n], yn_zreal_pos[n+1], yn_zreal_neg[n], yn_zreal_neg[n+1]])
+        ylow = np.amin([yn_zreal_pos[n], yn_zreal_pos[n+1], yn_zreal_neg[n], yn_zreal_neg[n+1]])
+        zup = np.amax([zn_real[n], zn_real[n+1]])+resolution
+        zlow = np.amin([zn_real[n], zn_real[n+1]])
 
-    zreal = z-zzx/1e3
-    xx_zreal = x + xx_z/1e3
-    plt.plot(zzx, xx_z, zx_a, x_za, zzy, yy_z, zy_a, y_za)
-    plt.axis('equal')
-    #plt.axis([0, 220, 0, 120])
-    plt.plot(zreal, xx_zreal)
-    plt.show()
+        # if xup-xlow and yup-ylow > 0 or xup-xlow and zup-zlow > 0 or yup-ylow and zup-zlow > 0:
+        #     plate(xlow, ylow, zlow, xup, yup, zup,'pec')
+
+        if (xup-xlow) and (yup-ylow) and (zup-zlow) > 0:
+            box(xlow, ylow, zlow, xup, yup, zup,'pec')
+
+        xup = np.amax([xn_zreal_neg[n], xn_zreal_neg[n+1]])
+        xlow = np.amin([xn_zreal_neg[n], xn_zreal_neg[n+1]])-resolution
+        # if xup-xlow and yup-ylow > 0 or xup-xlow and zup-zlow > 0 or yup-ylow and zup-zlow > 0:
+        #     plate(xlow, ylow, zlow, xup, yup, zup,'pec')
+
+        if (xup-xlow) and (yup-ylow) and (zup-zlow) > 0:
+            box(xlow, ylow, zlow, xup, yup, zup,'pec')
+
+    # radius   
+    for n in range(0, len(za_real)-1):
+        xup = np.amax([xa_real_pos[n], xa_real_pos[n+1]])+resolution
+        xlow = np.amin([xa_real_pos[n], xa_real_pos[n+1]])
+        yup = np.amax([ya_real_pos[n], ya_real_pos[n+1], ya_real_neg[n], ya_real_neg[n+1]])
+        ylow = np.amin([ya_real_pos[n], ya_real_pos[n+1], ya_real_neg[n], ya_real_neg[n+1]])
+        zup = np.amax([za_real[n], za_real[n+1]])#+resolution
+        zlow = np.amin([za_real[n], za_real[n+1]])-resolution
+
+        # if xup-xlow and yup-ylow > 0 or xup-xlow and zup-zlow > 0 or yup-ylow and zup-zlow > 0:
+        #     plate(xlow, ylow, zlow, xup, yup, zup,'pec')
+
+        if (xup-xlow) and (yup-ylow) and (zup-zlow) > 0:
+            box(xlow, ylow, zlow, xup, yup, zup,'pec')
+
+        xup = np.amax([xa_real_neg[n], xa_real_neg[n+1]])
+        xlow = np.amin([xa_real_neg[n], xa_real_neg[n+1]])-resolution
+
+        if (xup-xlow) and (yup-ylow) and (zup-zlow) > 0:
+            box(xlow, ylow, zlow, xup, yup, zup,'pec')
+        
 
     ### Source on SMA Connector ###
     tx = sma_center[0]+G/2+0.010+resolution, sma_center[1], sma_center[2]
@@ -125,7 +179,7 @@ def horn_burr(x, y, z, resolution = 0.0005, rotation = 0, measurement ='monostat
 
     ### Reciever on SMA Connector ###
     if measurement == 'monostatic':
-        rx = tx
+        #rx = tx
         identifier = 'monostatic'
     elif measurement == 'rx':
         identifier = 'rxhorn'
@@ -134,4 +188,4 @@ def horn_burr(x, y, z, resolution = 0.0005, rotation = 0, measurement ='monostat
     else:
         raise CmdInputError('This antenna have 3 measuremnt methods - tx, rx, monostatic')
     
-    #rx(tx[0] - 0.059, tx[1], tx[2], identifier=identifier, polarisation='y', dxdy=(resolution, resolution))
+    rx(tx[0], tx[1], tx[2], identifier=identifier)
--- user_libs/antennas/discretGeo.py
+++ user_libs/antennas/discretGeo.py
@@ -55,83 +55,80 @@ def disGeometryGprMax(x, y, z, resolution):
 
     return xn, yn, zn
 
-def createPoints(self, parameter_list):
-    """
-    docstring
-    """
-    pass
+# L           = 0.2               # m
+# L_plate     = 0.07
+# resolution  = 0.0005            # m
+# R           = 0.019978589752617
+# Rside       = 0.011
+# B           = 0.17                   # m
+# tol3d       = 0.0003
+# G           = 0.0023 + tol3d    # m
+# arcRadius   = 0.025            # m
+# W1          = 0.13              # m
+# wfeed       = 0.011 + tol3d          # m
+# C1          = ((W1*1e3/2) - (G*1e3/2))/(np.exp(R*L*1e3)-np.exp(R*0))
+# C1_side     = (B*1e3/2-wfeed*1e3/2)/(np.exp(Rside*L*1e3)-np.exp(Rside*0))
+# C2          = (G*1e3/2 * np.exp(R*L*1e3)-W1*1e3/2 * np.exp(R*0))/(np.exp(R*L*1e3)-np.exp(R*0))  # 0.259141  ( G / 2 mm * exp(R * L / 1 mm) - W1 / 2 mm * exp(0 oE) ) / ( exp(R * L / 1 mm) - exp(0 oE) )
+# C2_side     = (wfeed*1e3/2*np.exp(Rside*L*1e3)-B*1e3/2*np.exp(Rside*0))/(np.exp(Rside*L*1e3)-np.exp(Rside*0))
+# slopeAngel  = np.arctan(C1*R*np.exp(R*L*1e3))  # 52.291047
+# arcAngle    = np.deg2rad(90)                # deg
+# arcStart    = (slopeAngel - arcAngle)       # deg
+# arcStop     = arcAngle
+# arcDelta    = arcStop - arcStart
+# Lfeed       = 0.02                  # m
+# deltaB      = 0.02                  # m
+# B2          = B + deltaB            # m
+# lBalun      = 0.014                 # m
+# balunDiaBig = 0.011                 # m
+# balunDiaSmall = 0.008               # m
+# smaDia      = 0.001
+# smaTeflonDia= 0.005
+# smaSocketDia= 0.008
+# smaScrwDia  = 0.006
+# sourceresistance = 200              # ohm
 
-L           = 0.2               # m
-L_plate     = 0.07
-resolution  = 0.0005            # m
-R           = 0.019978589752617
-Rside       = 0.011
-B           = 0.17                   # m
-tol3d       = 0.0003
-G           = 0.0023 + tol3d    # m
-arcRadius   = 0.025            # m
-W1          = 0.13              # m
-wfeed       = 0.011 + tol3d          # m
-C1          = ((W1*1e3/2) - (G*1e3/2))/(np.exp(R*L*1e3)-np.exp(R*0))
-C1_side     = (B*1e3/2-wfeed*1e3/2)/(np.exp(Rside*L*1e3)-np.exp(Rside*0))
-C2          = (G*1e3/2 * np.exp(R*L*1e3)-W1*1e3/2 * np.exp(R*0))/(np.exp(R*L*1e3)-np.exp(R*0))  # 0.259141  ( G / 2 mm * exp(R * L / 1 mm) - W1 / 2 mm * exp(0 oE) ) / ( exp(R * L / 1 mm) - exp(0 oE) )
-C2_side     = (wfeed*1e3/2*np.exp(Rside*L*1e3)-B*1e3/2*np.exp(Rside*0))/(np.exp(Rside*L*1e3)-np.exp(Rside*0))
-slopeAngel  = np.arctan(C1*R*np.exp(R*L*1e3))  # 52.291047
-arcAngle    = np.deg2rad(90)                # deg
-arcStart    = (slopeAngel - arcAngle)       # deg
-arcStop     = arcAngle
-arcDelta    = arcStop - arcStart
-Lfeed       = 0.02                  # m
-deltaB      = 0.02                  # m
-B2          = B + deltaB            # m
-lBalun      = 0.014                 # m
-balunDiaBig = 0.011                 # m
-balunDiaSmall = 0.008               # m
-smaDia      = 0.001
-smaTeflonDia= 0.005
-smaSocketDia= 0.008
-smaScrwDia  = 0.006
-sourceresistance = 200              # ohm
+# print('C1:'+str(C1)+' C1side:'+str(C1_side)+' C2:'+str(C2)+' C2side:'+str(C2_side) + ' SlopeAngel:' + str(np.rad2deg(slopeAngel))+'\n'+'ArcStart:'+ str(np.rad2deg(arcStart)))
+# zl = np.arange(0, (L+resolution)*1e3, resolution*1e3)
+# alpha = np.linspace(arcStart, arcStop, num=200, endpoint=True)
+# # horn
+# x_z = C1 * np.exp((zl*R))+C2      # horn part
+# y_z = C1_side*np.exp(Rside*zl)+C2_side+(wfeed*1e3/2)-wfeed*1e3/2   # c1_side*efk^(R_side*v*u)+c2_side+(w_feed/2*v)-w_feed/2 # C1_SIDE*exp(R_SIDE*t)+C2_SIDE+(W_FEED/2mm)-W_FEED/2
+# x_alpha = W1*1e3/2+arcRadius*1e3*np.sin(alpha)+np.cos(slopeAngel)*arcRadius*1e3
+# y_alpha = B*1e3/2+deltaB*1e3/2*(alpha-arcStart)/arcDelta #B/2+(DELTA_B)/2*(t-ARC_START/1grd)/(ARC_DELTA/1grd)
+# z_alpha = L*1e3+arcRadius*1e3*np.cos(alpha)-np.sin(slopeAngel)*arcRadius*1e3
 
-print('C1:'+str(C1)+' C1side:'+str(C1_side)+' C2:'+str(C2)+' C2side:'+str(C2_side) + ' SlopeAngel:' + str(np.rad2deg(slopeAngel))+'\n'+'ArcStart:'+ str(np.rad2deg(arcStart)))
-zl = np.arange(0, (L+resolution)*1e3, resolution*1e3)
-alpha = np.linspace(arcStart, arcStop, num=200, endpoint=True)
-# horn
-x_z = C1 * np.exp((zl*R))+C2      # horn part
-y_z = C1_side*np.exp(Rside*zl)+C2_side+(wfeed*1e3/2)-wfeed*1e3/2   # c1_side*efk^(R_side*v*u)+c2_side+(w_feed/2*v)-w_feed/2 # C1_SIDE*exp(R_SIDE*t)+C2_SIDE+(W_FEED/2mm)-W_FEED/2
-x_alpah = W1*1e3/2+arcRadius*1e3*np.sin(alpha)+np.cos(slopeAngel)*arcRadius*1e3
-y_alpah = B*1e3/2+deltaB*1e3/2*(alpha-arcStart)/arcDelta #B/2+(DELTA_B)/2*(t-ARC_START/1grd)/(ARC_DELTA/1grd)
-z_alpha = L*1e3+arcRadius*1e3*np.cos(alpha)-np.sin(slopeAngel)*arcRadius*1e3
+# # discret funcion
+# zn, yn_z, xn_z = disGeometryGprMax(zl, y_z, x_z, resolution*1e3)
+# # zzx, xx_z = disGeometryGprMax(zl, x_z, resolution*1e3)
+# za, ya, xa = disGeometryGprMax(z_alpha, y_alpha, x_alpha, resolution*1e3)
+# # zy_a, y_za = disGeometryGprMax(z_alpha, y_alpah, resolution*1e3)
 
-# discret funcion
-zn, yn_z, xn_z = disGeometryGprMax(zl, y_z, x_z, resolution*1e3)
-# zzx, xx_z = disGeometryGprMax(zl, x_z, resolution*1e3)
-# zx_a, x_za = disGeometryGprMax(z_alpha, x_alpah, resolution*1e3)
-# zy_a, y_za = disGeometryGprMax(z_alpha, y_alpah, resolution*1e3)
+# # build geometry
 
-# build geometry
+# # zz = np.append(zzy, zzx)
+# # zz = np.sort(zz)
+# # print('zzy:'+str(zzy)+'\n zzx'+str(zzx)+'\n zconnect and sorted:'+str(np.unique(zz)))
 
-# zz = np.append(zzy, zzx)
-# zz = np.sort(zz)
-# print('zzy:'+str(zzy)+'\n zzx'+str(zzx)+'\n zconnect and sorted:'+str(np.unique(zz)))
+# plt.plot(zn, yn_z)
+# plt.show()
+# plt.plot(zn, xn_z)
+# plt.show()
 
-plt.plot(zn, yn_z)
-plt.show()
-plt.plot(zn, xn_z)
-plt.show()
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-plt.plot(zn, yn_z, xn_z)
-# ax.set_xlim3d(0,50)
-# ax.set_ylim3d(0,20)
-# ax.set_zlim3d(0,30)
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# #plt.plot(zn, yn_z, xn_z)
+# #plt.show()
+# #plt.plot(z_alpha, y_alpha, x_alpha)
+# plt.plot(za, ya, xa)
+# # ax.set_xlim3d(0,50)
+# # ax.set_ylim3d(0,20)
+# # ax.set_zlim3d(0,30)
 
 
-# zreal = z-zzx/1e3
-# xx_zreal = x + xx_z/1e3
-# plt.plot(zzx, xx_z, zx_a, x_za, zzy, yy_z, zy_a, y_za)
-# plt.axis('equal')
-#plt.axis([0, 220, 0, 120])
-# plt.plot(zreal, xx_zreal)
-plt.show()
+# # zreal = z-zzx/1e3
+# # xx_zreal = x + xx_z/1e3
+# # plt.plot(zzx, xx_z, zx_a, x_za, zzy, yy_z, zy_a, y_za)
+# # plt.axis('equal')
+# #plt.axis([0, 220, 0, 120])
+# # plt.plot(zreal, xx_zreal)
+# plt.show()
