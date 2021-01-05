ce0a3cfa79820eab6b475fb896663763e026676d
jasminium <johnmatthewhartley@gmail.com>
2021-01-05 11:53:11 +0100

    Round plate points before discretisation

--- gprMax/cmds_geometry/plate.py
+++ gprMax/cmds_geometry/plate.py
@@ -80,11 +80,12 @@ class Plate(UserObjectGeometry):
         if self.dorotate:
             self.__dorotate()
 
+        p3 = uip.round_to_grid_static_point(p1)
+        p4 = uip.round_to_grid_static_point(p2)
+
         p1, p2 = uip.check_box_points(p1, p2, self.__str__())
         xs, ys, zs = p1
         xf, yf, zf = p2
-        p3 = uip.round_to_grid_static_point(p1)
-        p4 = uip.round_to_grid_static_point(p2)
 
         # Check for valid orientations
         if xs == xf:
