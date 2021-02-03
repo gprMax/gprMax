8316486d3728b2b04ef43c7c5fa4dd6168151f5f
yuanzi1501040205 <ziyuan1501040205@gmail.com>
2021-02-03 13:09:28 -0600

    plot shot gather scan of the fixed transmitting antenna and a receiver array(multiple receivers)

---
+++ tools/plot_ShotGather.py
@@ -0,0 +1,103 @@
+# Copyright (C) 2015-2020: The University of Edinburgh
+#                 Authors: Craig Warren and Antonis Giannopoulos
+#
+# This file is part of gprMax.
+#
+# gprMax is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# gprMax is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gprMax.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+import os
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+
+from gprMax.exceptions import CmdInputError
+from .outputfiles_merge import get_output_data
+
+
+def mpl_plot(filename, outputdata, dt, rxnumber, rxcomponent):
+    """Creates a plot (with matplotlib) of the Shot Gather.
+
+    Args:
+        filename (string): Filename (including path) of output file.
+        outputdata (array): Array of Shot Gather.
+        dt (float): Temporal resolution of the model.
+        rxnumber (int): Receiver output number.
+        rxcomponent (str): Receiver output field/current component.
+
+    Returns:
+        plt (object): matplotlib plot object.
+    """
+
+    (path, filename) = os.path.split(filename)
+
+    fig = plt.figure(num=filename + ' - rx' + str(rxnumber), 
+                     figsize=(20, 10), facecolor='w', edgecolor='w')
+    plt.imshow(outputdata, 
+               extent=[0, outputdata.shape[1], outputdata.shape[0] * dt, 0],
+               interpolation='nearest', aspect='auto', cmap='seismic', 
+               vmin=-np.amax(np.abs(outputdata)), vmax=np.amax(np.abs(outputdata)))
+    plt.xlabel('Trace number')
+    plt.ylabel('Time [s]')
+    # plt.title('{}'.format(filename))
+
+    # Grid properties
+    ax = fig.gca()
+    ax.grid(which='both', axis='both', linestyle='-.')
+
+    cb = plt.colorbar()
+    if 'E' in rxcomponent:
+        cb.set_label('Field strength [V/m]')
+    elif 'H' in rxcomponent:
+        cb.set_label('Field strength [A/m]')
+    elif 'I' in rxcomponent:
+        cb.set_label('Current [A]')
+
+    # Save a PDF/PNG of the figure
+    # savefile = os.path.splitext(filename)[0]
+    # fig.savefig(path + os.sep + savefile + '.pdf', dpi=None, format='pdf', 
+    #             bbox_inches='tight', pad_inches=0.1)
+    # fig.savefig(path + os.sep + savefile + '.png', dpi=150, format='png', 
+    #             bbox_inches='tight', pad_inches=0.1)
+
+    return plt
+
+
+if __name__ == "__main__":
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Plots a Shot-gather image.',
+                                     usage='cd gprMax; python -m tools.plot_ShotGather outputfile output')
+    parser.add_argument('outputfile', help='name of output file including path')
+    parser.add_argument('rx_component', help='name of output component to be plotted', 
+                        choices=['Ex', 'Ey', 'Ez', 'Hx', 'Hy', 'Hz', 'Ix', 'Iy', 'Iz'])
+    args = parser.parse_args()
+
+    # Open output file and read number of outputs (receivers)
+    f = h5py.File(args.outputfile, 'r')
+    nrx = f.attrs['nrx']
+    f.close()
+
+    # Check there are any receivers
+    if nrx == 0:
+        raise CmdInputError('No receivers found in {}'.format(args.outputfile))
+    img = []
+    for rx in range(1, nrx + 1):
+        outputdata, dt = get_output_data(args.outputfile, rx, args.rx_component)
+        img.append(outputdata)
+    img = np.array(img).transpose()
+
+    plthandle = mpl_plot(args.outputfile, img, dt, rx, args.rx_component)
+    plthandle.show()
