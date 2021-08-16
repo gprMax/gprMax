24fff287a55c5683f6799d49c773ad0a19b0d0e2
utsav-akhaury <f2016684@pilani.bits-pilani.ac.in>
2021-08-16 12:08:05 +0200

    Added PCA as the default method for compression

--- ML/ML_utilities.py
+++ ML/ML_utilities.py
@@ -1,3 +1,21 @@
+# Authors: Iraklis Giannakis and Utsav Akhaury
+#
+# This file is part of gprMax.
+#
+# gprMax is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# gprMax is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gprMax.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,7 +23,7 @@ from sklearn.decomposition import PCA
 from sklearn.decomposition import TruncatedSVD
 
 
-def apply_compression(input_data, err_tolerance, alg):
+def apply_compression(input_data, err_tolerance, alg='pca'):
 
     """ Compresses the input data based on the specified algorithm
         
