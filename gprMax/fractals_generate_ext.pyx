368e7c16e38bf49c6a92608e8aa74efb3f35ece4
craig-warren <craig_warren@me.com>
2024-01-03 11:20:54 +0000

    Update to fractal behaviour. Fixes int/float bug & D now used directly.

--- gprMax/fractals_generate_ext.pyx
+++ gprMax/fractals_generate_ext.pyx
@@ -21,13 +21,13 @@ cimport numpy as np
 from cython.parallel import prange
 
 
-cpdef void generate_fractal2D(int nx, int ny, int nthreads, int b, np.float64_t[:] weighting, np.float64_t[:] v1, np.complex128_t[:, ::1] A, np.complex128_t[:, ::1] fractalsurface):
+cpdef void generate_fractal2D(int nx, int ny, int nthreads, float D, np.float64_t[:] weighting, np.float64_t[:] v1, np.complex128_t[:, ::1] A, np.complex128_t[:, ::1] fractalsurface):
     """This function generates a fractal surface for a 2D array.
         
     Args:
         nx, ny (int): Fractal surface size in cells
         nthreads (int): Number of threads to use
-        b (int): Constant related to fractal dimension
+        D (float): Fractal dimension
         weighting (memoryview): Access to weighting vector
         v1 (memoryview): Access to positional vector at centre of array, scaled by weighting
         A (memoryview): Access to array containing random numbers (to be convolved with fractal function)
@@ -46,20 +46,20 @@ cpdef void generate_fractal2D(int nx, int ny, int nthreads, int b, np.float64_t[
                 # Calulate norm of v2 - v1
                 rr = ((v2x - v1[0])**2 + (v2y - v1[1])**2)**(1/2)
 
-                B = rr**b
+                B = rr**D
                 if B == 0:
                     B = 0.9
 
                 fractalsurface[i, j] = A[i, j] / B
 
 
-cpdef void generate_fractal3D(int nx, int ny, int nz, int nthreads, int b, np.float64_t[:] weighting, np.float64_t[:] v1, np.complex128_t[:, :, ::1] A, np.complex128_t[:, :, ::1] fractalvolume):
+cpdef void generate_fractal3D(int nx, int ny, int nz, int nthreads, float D, np.float64_t[:] weighting, np.float64_t[:] v1, np.complex128_t[:, :, ::1] A, np.complex128_t[:, :, ::1] fractalvolume):
     """This function generates a fractal volume for a 3D array.
 
     Args:
         nx, ny, nz (int): Fractal volume size in cells
         nthreads (int): Number of threads to use
-        b (int): Constant related to fractal dimension
+        D (float): Fractal dimension
         weighting (memoryview): Access to weighting vector
         v1 (memoryview): Access to positional vector at centre of array, scaled by weighting
         A (memoryview): Access to array containing random numbers (to be convolved with fractal function)
@@ -79,7 +79,7 @@ cpdef void generate_fractal3D(int nx, int ny, int nz, int nthreads, int b, np.fl
 
                 # Calulate norm of v2 - v1
                 rr = ((v2x - v1[0])**2 + (v2y - v1[1])**2 + (v2z - v1[2])**2)**(1/2)
-                B = rr**b
+                B = rr**D
                 if B == 0:
                     B = 0.9 
                     
