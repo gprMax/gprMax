60923f50f3baeb034f2f10976c0090b4f592fbd7
Craig Warren <craig_warren@me.com>
2024-04-24 21:32:39 +0100

    Need to clear scenes list when looped API call to gprMax.run

--- gprMax/contexts.py
+++ gprMax/contexts.py
@@ -63,8 +63,9 @@ class Context:
         elif config.sim_config.general["solver"] == "opencl":
             print_opencl_info(config.sim_config.devices["devs"])
 
-        # Clear list of model configs. It can be retained when gprMax is
-        # called in a loop, and want to avoid this.
+        # Clear list of scenes and model configs, which can be retained when 
+        # gprMax is called in a loop, and want to avoid this.
+        config.sim_config.scenes = []
         config.model_configs = []
 
         for i in self.model_range:
