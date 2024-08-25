97c6dcc237c23e8f1437129fd57298e5d2ef2c0c
YTchen <james2002chen@gmail.com>
2024-08-26 00:45:34 +0800

    multiple z loops

--- gprMax/pybind11/xpu_update_normal.cpp
+++ gprMax/pybind11/xpu_update_normal.cpp
@@ -13,17 +13,18 @@ void xpu_update::update_electric_normal(update_range_t update_range) {
         for (int j = y_range.first; j < y_range.second; j++) {
             for (int k = z_range.first; k < z_range.second; k++) {
                 int materialEx = ID_[INDEX_ID(0, i, j, k)];
-                int materialEy = ID_[INDEX_ID(1, i, j, k)];
-                int materialEz = ID_[INDEX_ID(2, i, j, k)];
-
                 Ex_[INDEX(i, j, k)] = (updatecoeffsE_[materialEx * 5 + 0] * Ex_[INDEX(i, j, k)] +
                                        updatecoeffsE_[materialEx * 5 + 2] * (Hz_[INDEX(i, j, k)] - Hz_[INDEX(i, j - 1, k)]) -
                                        updatecoeffsE_[materialEx * 5 + 3] * (Hy_[INDEX(i, j, k)] - Hy_[INDEX(i, j, k - 1)]));
-
+            }
+            for (int k = z_range.first; k < z_range.second; k++) {
+                int materialEy = ID_[INDEX_ID(1, i, j, k)];
                 Ey_[INDEX(i, j, k)] = (updatecoeffsE_[materialEy * 5 + 0] * Ey_[INDEX(i, j, k)] +
                                        updatecoeffsE_[materialEy * 5 + 3] * (Hx_[INDEX(i, j, k)] - Hx_[INDEX(i, j, k - 1)]) -
                                        updatecoeffsE_[materialEy * 5 + 1] * (Hz_[INDEX(i, j, k)] - Hz_[INDEX(i - 1, j, k)]));
-
+            }
+            for (int k = z_range.first; k < z_range.second; k++) {
+                int materialEz = ID_[INDEX_ID(2, i, j, k)];
                 Ez_[INDEX(i, j, k)] = (updatecoeffsE_[materialEz * 5 + 0] * Ez_[INDEX(i, j, k)] +
                                        updatecoeffsE_[materialEz * 5 + 1] * (Hy_[INDEX(i, j, k)] - Hy_[INDEX(i - 1, j, k)]) -
                                        updatecoeffsE_[materialEz * 5 + 2] * (Hx_[INDEX(i, j, k)] - Hx_[INDEX(i, j - 1, k)]));
@@ -46,17 +47,18 @@ void xpu_update::update_magnetic_normal(update_range_t update_range) {
         for (int j = y_range.first; j < y_range.second; j++) {
             for (int k = z_range.first; k < z_range.second; k++) {
                 int materialHx = ID_[INDEX_ID(3, i, j, k)];
-                int materialHy = ID_[INDEX_ID(4, i, j, k)];
-                int materialHz = ID_[INDEX_ID(5, i, j, k)];
-
                 Hx_[INDEX(i, j, k)] = (updatecoeffsH_[materialHx * 5 + 0] * Hx_[INDEX(i, j, k)] -
                                        updatecoeffsH_[materialHx * 5 + 2] * (Ez_[INDEX(i, j + 1, k)] - Ez_[INDEX(i, j, k)]) +
                                        updatecoeffsH_[materialHx * 5 + 3] * (Ey_[INDEX(i, j, k + 1)] - Ey_[INDEX(i, j, k)]));
-
+            }
+            for (int k = z_range.first; k < z_range.second; k++) {
+                int materialHy = ID_[INDEX_ID(4, i, j, k)];
                 Hy_[INDEX(i, j, k)] = (updatecoeffsH_[materialHy * 5 + 0] * Hy_[INDEX(i, j, k)] -
                                        updatecoeffsH_[materialHy * 5 + 3] * (Ex_[INDEX(i, j, k + 1)] - Ex_[INDEX(i, j, k)]) +
                                        updatecoeffsH_[materialHy * 5 + 1] * (Ez_[INDEX(i + 1, j, k)] - Ez_[INDEX(i, j, k)]));
-
+            }
+            for (int k = z_range.first; k < z_range.second; k++) {
+                int materialHz = ID_[INDEX_ID(5, i, j, k)];
                 Hz_[INDEX(i, j, k)] = (updatecoeffsH_[materialHz * 5 + 0] * Hz_[INDEX(i, j, k)] -
                                        updatecoeffsH_[materialHz * 5 + 1] * (Ey_[INDEX(i + 1, j, k)] - Ey_[INDEX(i, j, k)]) +
                                        updatecoeffsH_[materialHz * 5 + 2] * (Ex_[INDEX(i, j + 1, k)] - Ex_[INDEX(i, j, k)]));
