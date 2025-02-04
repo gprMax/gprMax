845c75daab88ac957e10e31e0ebc89d43771585f
Craig Warren <craig-warren@me.com>
2025-02-04 16:50:10 +0000

    Copyright update

--- testing/models_pmls/pml_basic/plot_pml_comparison.py
+++ testing/models_pmls/pml_basic/plot_pml_comparison.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2024: The University of Edinburgh, United Kingdom
+# Copyright (C) 2015-2025: The University of Edinburgh, United Kingdom
 #                 Authors: Craig Warren, Antonis Giannopoulos, and John Hartley
 #
 # This file is part of gprMax.
