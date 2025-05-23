8e4f1565950eb9e2b87822e51d17d3351c9a5b15
Nathan Mannall <n.mannall@epcc.ed.ac.uk>
2025-05-23 17:19:47 +0100

    Update reading geometry objects for parallel build

--- reframe_tests/tests/test_geometry_objects.py
+++ reframe_tests/tests/test_geometry_objects.py
@@ -30,7 +30,6 @@ class TestGeometryObjectReadFullVolume(ReceiverMixin, GprMaxGeometryObjectsReadT
     sourcesdir = "src/geometry_object_tests"
     model = parameter(["geometry_object_read_full_volume"])
     geometry_objects_read = {"full_volume": "full_volume_read"}
-    geometry_objects_write = ["full_volume"]
     test_dependency = TestGeometryObject
 
 
@@ -45,9 +44,11 @@ class TestGeometryObjectReadFullVolumeMPI(MpiMixin, TestGeometryObjectReadFullVo
 class TestGeometryObjectReadWrite(GeometryOnlyMixin, GprMaxGeometryObjectsReadWriteTest):
     tags = {"test", "serial", "geometry only", "geometry object"}
     sourcesdir = "src/geometry_object_tests"
-    model = parameter(["geometry_object_read_write_partial_volume"])
-    geometry_objects_read = {"partial_volume": "partial_volume_read"}
-    geometry_objects_write = ["partial_volume"]
+    model = parameter(["geometry_object_read_write"])
+    geometry_objects_read = {
+        "full_volume": "full_volume_read",
+    }
+    geometry_objects_write = ["partial_volume", "full_volume"]
     test_dependency = TestGeometryObject
 
 
@@ -55,4 +56,25 @@ class TestGeometryObjectReadWrite(GeometryOnlyMixin, GprMaxGeometryObjectsReadWr
 class TestGeometryObjectReadWriteMPI(MpiMixin, TestGeometryObjectReadWrite):
     tags = {"test", "mpi", "geometry only", "geometry object"}
     mpi_layout = parameter([[2, 2, 2], [4, 4, 1]])
+    test_dependency = TestGeometryObject
+
+
+# TODO: This test fails in the serial implementation due to the geometry
+# object being positioned such that it overflows the grid
+# @rfm.simple_test
+class TestGeometryObjectMove(GeometryOnlyMixin, GprMaxGeometryObjectsReadWriteTest):
+    tags = {"test", "serial", "geometry only", "geometry object"}
+    sourcesdir = "src/geometry_object_tests"
+    model = parameter(["geometry_object_move"])
+    geometry_objects_read = {
+        "full_volume": "full_volume_read",
+    }
+    geometry_objects_write = ["partial_volume"]
+    test_dependency = TestGeometryObject
+
+
+@rfm.simple_test
+class TestGeometryObjectMoveMPI(MpiMixin, TestGeometryObjectMove):
+    tags = {"test", "mpi", "geometry only", "geometry object"}
+    mpi_layout = parameter([[2, 2, 2], [4, 3, 1]])
     test_dependency = TestGeometryObject
