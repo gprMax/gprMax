024748428bc695c4be71f3e95409b09d619f48f7
Anjie Hou <anjiehou@sjtu.edu.cn>
2025-08-23 14:09:37 +0000

    misc: clean the code

--- run.py
+++
@@ -1,8 +0,0 @@
-#!/astrum/home/hpchaj/apps/miniconda3/envs/gprMax-devel/bin/python
-import gprMax
-
-def main():
-    gprMax.gprMax.run(inputfile='/astrum/home/hpchaj/repos/gprMax/testing/benchmarking/700.in', hip=[1])
-
-if __name__ == "__main__":
-    main()
