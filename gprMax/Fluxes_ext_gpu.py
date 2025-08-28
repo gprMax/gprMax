535d835a19b6656afffdd771b88fce46780b9e82
QuentinDavid <quentin.david@quandela.com>
2025-08-28 12:47:26 +0200

    Cleaning Fluxes_ext_gpu from the tests and commentary

--- gprMax/Fluxes_ext_gpu.py
+++ gprMax/Fluxes_ext_gpu.py
@@ -30,9 +30,6 @@ __global__ void save_fields_flux(int NF, int NX, int NY, int NZ,
                                    
     // Obtain the linear index corresponding to the current thread
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-                                    
-    //printf("idx= %d", idx);
-    //printf("x_begin= %d, y_begin= %d, z_begin= %d \\n", $x_begin, $y_begin, $z_begin);
 
     assert($NX_FLUX == NX);
     assert($NY_FLUX == NY);
@@ -42,37 +39,20 @@ __global__ void save_fields_flux(int NF, int NX, int NY, int NZ,
     int i_fft = idx / (NY * NZ);
     int j_fft = (idx % (NY * NZ)) / NZ;
     int k_fft = (idx % (NY * NZ)) % NZ;
-                                    
-    //printf("x_begin= %d, y_begin= %d, z_begin= %d \\n", x_begin, y_begin, z_begin);
 
                                     
     // Convert the linear index to subscripts for 3D field arrays
     int i = i_fft + x_begin;
-    //printf("i= %d\\n", i);
     int j = j_fft + y_begin;
-    //printf("j= %d\\n", j);
     int k = k_fft + z_begin;
-    //printf("k= %d\\n", k);
-                                    
-    //printf("i= %d, j= %d, k= %d \\n", i, j, k);
-    
-    //if (idx == 0) printf("Test Ez[ID(50,50,50)]= %e \\n", Ez[INDEX3D_FIELDS(50,50,50)]);
-    //if (idx == 0) printf("x_begin= %d, y_begin= %d, z_begin= %d \\n", x_begin, y_begin, z_begin);
                                     
                                     
     float $SIN_OMEGA, $COS_OMEGA;
     int $IDX_5DX, $IDX_5DY, $IDX_5DZ;
 
     if (i_fft >= NX || j_fft >= NY || k_fft >= NZ) return;
-
-    //printf("\\n");
                                     
     int $IDX_3D = INDEX3D_FIELDS(i, j, k);
-                                    
-    //printf("NX_FIELDS = $NX_FIELDS, NY_FIEDS= $NY_FIELDS, NZ_FIELDS= $NZ_FIELDS \\n");
-    //printf("NX_FLUX = %d, NY_FLUX= %d, NZ_FLUX= %d \\n", NX, NY, NZ);
-                                    
-    //printf("i_fft= %d, j_fft= %d, k_fft= %d ---- INDEX_3D= %d \\n", i_fft, j_fft, k_fft, idx_3d);
 
                                     
     float $NORM = dt / sqrt(2 * CUDART_PI);
@@ -82,16 +62,11 @@ __global__ void save_fields_flux(int NF, int NX, int NY, int NZ,
         $REAL $PHASE = $OMEGA[f]*iteration*dt;
                                           
         sincosf($PHASE, &$SIN_OMEGA, &$COS_OMEGA);      
-        $COMPLEX $EXP_FACTOR($REAL($COS_OMEGA * $NORM), $REAL($SIN_OMEGA * $NORM));     
-        //printf("%e", $EXP_FACTOR.real());             
+        $COMPLEX $EXP_FACTOR($REAL($COS_OMEGA * $NORM), $REAL($SIN_OMEGA * $NORM));        
 
         $IDX_5DX = INDEX5D_FFT(f,i_fft,j_fft,k_fft, 0);
         $IDX_5DY = INDEX5D_FFT(f,i_fft,j_fft,k_fft, 1);
-        $IDX_5DZ = INDEX5D_FFT(f,i_fft,j_fft,k_fft, 2);   
-                                    
-    //    printf("i_fft= %d, j_fft= %d, k_fft= %d ---- INDEX_5D_x= %d ---- INDEX_5D_y= %d ---- INDEX_5D_z= %d \\n", i_fft, j_fft, k_fft, $IDX_5DX, $IDX_5DY, $IDX_5DZ);
-    //    printf("idx_3d= %d", $IDX_3D);
-    //    printf("%e", Ex[$IDX_3D]);
+        $IDX_5DZ = INDEX5D_FFT(f,i_fft,j_fft,k_fft, 2);
     
         FFT_E[$IDX_5DX] += Ex[$IDX_3D] * $EXP_FACTOR;  
         FFT_E[$IDX_5DY] += Ey[$IDX_3D] * $EXP_FACTOR;
@@ -100,13 +75,5 @@ __global__ void save_fields_flux(int NF, int NX, int NY, int NZ,
         FFT_H[$IDX_5DX] += Hx[$IDX_3D] * $EXP_FACTOR;
         FFT_H[$IDX_5DY] += Hy[$IDX_3D] * $EXP_FACTOR;
         FFT_H[$IDX_5DZ] += Hz[$IDX_3D] * $EXP_FACTOR;
-        if (0 == 1 && iteration >= 300){                               
-            printf("Ex= %e \\n", Ex[$IDX_3D]);
-            printf("Ey= %e \\n", Ey[$IDX_3D]);
-            printf("Ez= %e \\n", Ez[$IDX_3D]);                              
-            printf("Hx= %e \\n", Hx[$IDX_3D]);
-            printf("Hy= %e \\n", Hy[$IDX_3D]);
-            printf("Hz= %e \\n", Hz[$IDX_3D]);}
-        }
     }
 """)
